b97beb4419d28c67868c4e91ef3c466c3f270f86
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-05 19:24:29 -0700

    Added a few more lines and renamed two of the existing classes under html. Edited/added more lines in fetching and  generating weather forecast.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,35 @@
 // Fetching html elements
-const currentWeather = document.getElementsByClassName('present-day-weather');
+const searchButton = document.querySelector('.search-button');
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=a4f68e66624f545f19a69eefc5107917')
-    .then(response => response.json())
-    .then(data => console.log(data))
+// Passing additional information for a more tailored response
+let weatherSearch = {
+    fetchWeather: function (city) {
+        fetch(
+            'https://api.openweathermap.org/data/2.5/weather?q='
+            + city
+            + "&units=imperial&appid=a4f68e66624f545f19a69eefc5107917"
+        )
+            .then(response => response.json())
+            .then(data => this.weather(data))
+    },
+    weather: function (data) {
+        const { name } = data;
+        // add The date
+        const { icon } = data.weather[0];
+        const { temp } = data.main;
+        const { speed } = data.wind;
+        const { humidity } = data.main;
+        console.log(name, temp, icon, speed, humidity);
+        document.querySelector('.city-name').innerText = name;
+        document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector('.temp').innerText = "Temp: " + temp + " ℉";
+        document.querySelector('.wind').innerText = "Wind: " + speed + " MPH";
+        document.querySelector('.hum').innerText = "Humidity: " + humidity + " %";
+    },
+    userInput: function () {
+        this.fetchWeather(document.querySelector('.user-input').value);
+    }
+}
+searchButton.addEventListener('click', function(){
+    weatherSearch.userInput();
+})
