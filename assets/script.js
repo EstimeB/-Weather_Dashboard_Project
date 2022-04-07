a538c3c64509f9e77667065475bb4cdda91739e6
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-06 17:57:31 -0700

    added new lines of codes and got the five weather forecast working and added a card (from bootstrap).

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Fetching html elements
 const searchButton = document.querySelector('.search-button');
+const weatherList = document.querySelector('.weather-list');
 
-// Passing additional information for a more tailored response
 let weatherSearch = {
     fetchWeather: function (city) {
         fetch(
@@ -19,7 +19,7 @@ let weatherSearch = {
         const { temp } = data.main;
         const { speed } = data.wind;
         const { humidity } = data.main;
-        console.log(name, temp, icon, speed, humidity);
+        // console.log(name, temp, icon, speed, humidity);
         document.querySelector('.city-name').innerText = name;
         document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector('.temp').innerText = "Temp: " + temp + " ℉";
@@ -28,8 +28,45 @@ let weatherSearch = {
     },
     userInput: function () {
         this.fetchWeather(document.querySelector('.user-input').value);
-    }
+        this.fetchWeatherForecast(document.querySelector('.user-input').value);
+    },
+    fetchWeatherForecast: function (city) {
+        fetch(
+            'https://api.openweathermap.org/data/2.5/forecast?q='
+            + city
+            + '&units=imperial&appid=a4f68e66624f545f19a69eefc5107917'
+        )
+            .then(response => response.json())
+            .then(data => this.weatherForecastDisplay(data))
+    },
+    weatherForecastDisplay: function (data) {
+        console.log(data)
+        let values = data.list
+        let forecastHTML = "";
+        for (i = 0; i < values.length; i=i+8) {
+            console.log(values[i]);
+            // add The date
+            const { icon } = values[i].weather[0];
+            const { temp } = values[i].main;
+            const { speed } = values[i].wind;
+            const { humidity } = values[i].main;
+            forecastHTML += `<div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text">Temp : ${temp}
+              <img src="https://openweathermap.org/img/wn/${icon}.png" alt="...">
+              </p>
+              <p class="Card-text">Humiditiy: ${humidity} " %"</p>
+              <p class="card-text">Wind: ${speed} " MPH"</p>
+            </div>
+          </div>`
+            // let forecast = document.createElement('li').innerText = "Temp: "+ temp + " ℉";
+            // weatherList.append(forecast);
+            weatherList.innerHTML =forecastHTML;
+        }
+    },
 }
-searchButton.addEventListener('click', function(){
+searchButton.addEventListener('click', function () {
     weatherSearch.userInput();
+    // weatherForecastSearch.userInputTwo();
 })
+
