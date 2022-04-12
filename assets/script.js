290dfca9e50e75ec460e02bb9fc059777bd5d98d
Brithnie Estime <estimebrithnie@yahoo.com>
2022-04-12 08:28:44 -0700

    (html)Remove few line and added new to accomodate styleshee t. (css)Added styling properties/features. (js)Used moment js in adding and reating a for loop for the weather dashboard dat e.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+let currentDay = moment();
 // Fetching html elements
 const searchButton = document.querySelector('.search-button');
 const weatherList = document.querySelector('.weather-list');
@@ -14,22 +15,23 @@ let weatherSearch = {
     },
     weather: function (data) {
         const { name } = data;
-        // add The date
         const { icon } = data.weather[0];
         const { temp } = data.main;
         const { speed } = data.wind;
         const { humidity } = data.main;
-        // console.log(name, temp, icon, speed, humidity);
+        console.log(name, temp, icon, speed, humidity);
         document.querySelector('.city-name').innerText = name;
+        document.querySelector('.date').append(currentDay.format('D/M/YYYY'));
         document.querySelector('.icon').src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector('.temp').innerText = "Temp: " + temp + " ℉";
         document.querySelector('.wind').innerText = "Wind: " + speed + " MPH";
         document.querySelector('.hum').innerText = "Humidity: " + humidity + " %";
-        const lat= data.coord.lat
+        const lat = data.coord.lat
         const lon = data.coord.lon
-        this.uvdata(lat,lon)
+        this.uvdata(lat, lon);
     },
-    uvdata:function(lat,lon){
+
+    uvdata: function (lat, lon) {
         fetch(
             "https://api.openweathermap.org/data/2.5/onecall?lat="
             + lat
@@ -48,7 +50,7 @@ let weatherSearch = {
         this.fetchWeatherForecast(document.querySelector('.user-input').value);
         let previousHistory = JSON.parse(localStorage.getItem("WeatherDashboard")) || []
         previousHistory.push(document.querySelector('.user-input').value);
-        localStorage.setItem("WeatherDashboard",JSON.stringify(previousHistory));
+        localStorage.setItem("WeatherDashboard", JSON.stringify(previousHistory));
     },
     fetchWeatherForecast: function (city) {
         fetch(
@@ -61,9 +63,15 @@ let weatherSearch = {
     },
     weatherForecastDisplay: function (data) {
         console.log(data)
-        let values = data.list
+        let values = data.list;
         let forecastHTML = "";
-        for (i = 0; i < values.length; i=i+8) {
+        for (i = 0; i <= 5; i++) {
+            const day = currentDay.add(1, 'days');
+            const dt = day.format('D/M/YYYY');
+            forecastHTML += `<p class="dt">${dt}</p>`
+            console.log(forecastHTML);
+        }
+        for (i = 0; i < values.length; i = i + 8) {
             console.log(values[i]);
             // add The date
             const { icon } = values[i].weather[0];
@@ -72,22 +80,23 @@ let weatherSearch = {
             const { humidity } = values[i].main;
             forecastHTML += `<div class="card" style="width: 18rem;">
             <div class="card-body">
-              <p class="card-text">Temp : ${temp}
               <img src="https://openweathermap.org/img/wn/${icon}.png" alt="...">
-              </p>
-              <p class="Card-text">Humiditiy: ${humidity} " %"</p>
-              <p class="card-text">Wind: ${speed} " MPH"</p>
+              <p class="card-text">Temp : ${temp} ℉</p>
+              <p class="Card-text">Humiditiy: ${humidity} %</p>
+              <p class="card-text">Wind: ${speed} MPH</p>
             </div>
           </div>`
-            weatherList.innerHTML =forecastHTML;
+            weatherList.innerHTML = forecastHTML;
         }
     },
 }
 searchButton.addEventListener('click', function () {
     weatherSearch.userInput();
+    // localStorage();
 })
 
 
-
 // function localStorage() {
+//     const lastInput = localStorage.getItem(userInput);
+//     userInputSpan.textContent = lastInput;
 // }
